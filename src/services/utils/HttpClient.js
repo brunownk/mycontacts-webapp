6ad6e11d8177a9cf3b1ed1157d7912c38b7595c5
Bruno Neckel <neckel.bw@gmail.com>
2022-11-05 18:10:35 -0300

    add: custom error type

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -1,4 +1,5 @@
 import delay from '../../utils/delay';
+import APIError from '../../errors/APIError';
 
 class HttpClient {
   constructor(baseURL) {
@@ -6,11 +7,23 @@ class HttpClient {
   }
 
   async get(path) {
+    delay(500);
+
     const response = await fetch(`${this.baseURL}${path}`);
 
-    await delay(500);
+    const contentType = response.headers.get('Content-Type');
 
-    return response.json();
+    let body = null;
+
+    if (contentType.includes('application/json')) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+
+    throw new APIError(response, body);
   }
 }
 
