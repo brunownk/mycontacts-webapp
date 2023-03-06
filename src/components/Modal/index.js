a18c2e924592416bd06a28e0086db416e434e8d6
Bruno Neckel <neckel.bw@gmail.com>
2023-03-06 00:10:11 -0300

    add: exit animation of a modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
 import Button from '../Button';
 import ReactPortal from '../ReactPortal';
@@ -16,14 +17,34 @@ export default function Modal({
   onCancel,
   onConfirm,
 }) {
-  if (!visible) {
+  const [shouldRender, setShouldRender] = useState(visible);
+
+  useEffect(() => {
+    if (visible === true) {
+      setShouldRender(true);
+    }
+
+    let timeoutId;
+
+    if (visible === false) {
+      timeoutId = setTimeout(() => {
+        setShouldRender(false);
+      }, 300);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [visible]);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="modal-root">
-      <Overlay>
-        <Container danger={danger}>
+      <Overlay isLeaving={!visible}>
+        <Container danger={danger} isLeaving={!visible}>
           <h1>{title}</h1>
 
           <div className="modal-body">
